5650d66aa11b017f03554fcbe71ddd887383384d
Marilena Kosma <150340273+marilenakosma@users.noreply.github.com>
2026-05-24 23:00:10 +0300

    Added flask server for displaying scores

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask,jsonify,request
+from db import get_scores,save_score
+
+app = Flask(__name__)
+
+@app.route('/scores/<username>', methods=['GET'])
+
+def fetch_scores(username):
+    return jsonify(get_scores(username))
+
+if __name__== '__main__':
+    app.run(port=5000)
